df7ea7ed5872f929d64329496731667f4a90641e
saleemcraft <saleemshajahan03@gmail.com>
2026-04-03 08:26:03 +0530

    laravel login page

---
+++ LoginRegistrationLogout/routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+use App\Http\Controllers\Auth\AuthController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('login', [AuthController::class,'index'])->name('login'); 
+Route::get('registration', [AuthController::class,'registration'])->name('registration'); 
+Route::post('post-registration', [AuthController::class,'postRegistration'])->name('registration.post');
+Route::post('post-login', [AuthController::class,'postLogin'])->name('login.post');
+Route::get('dashboard', [AuthController::class,'dashboard'])->name('dashboard'); 
+Route::get('logout', [AuthController::class,'logout'])->name('logout'); 
+
